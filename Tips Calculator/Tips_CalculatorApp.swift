6b672b0a7da2d9ee94825e28394d257e1c85117a
flaminshinjan <srikpatra.2008@gmail.com>
2022-11-24 11:29:12 +0530

    Initial Commit

---
+++ Tips Calculator/Tips_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tips_CalculatorApp.swift
+//  Tips Calculator
+//
+//  Created by Shinjan Patra on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Tips_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
