93fd205dfdef4a6fb0e6bc242f64aad3ec89c9e6
flaminshinjan <srikpatra.2008@gmail.com>
2022-11-30 15:33:53 +0530

    New Project

--- Tips Calculator/ContentView.swift
+++ Tips Calculator/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = "100"
+    @State var tipPercent = 15.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("money_aesthetic")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            VStack {
+                HStack {
+                    Image(systemName: "dollarsign.circle.fill")
+                        .imageScale(.large)
+                        .foregroundColor(.brown)
+                        .font(.title)
+                    Text("Tip Calculator")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                HStack {
+                    Text("Rs")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    TextField("Amount" ,text: $total)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+                HStack{
+                    Slider(value: $tipPercent, in: 1...99, step: 1.0)
+                    Text("\(Int(tipPercent))")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    Text("%")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                   
+                }
+                if let totalNumber = Double(total){
+                    Text("Tip Amount: Rs\(totalNumber * tipPercent/100, specifier: "%0.2f")")
+                        .fontWeight(.bold)
+                    .foregroundColor(.white)
+                }else {
+                    Text("Please enter a numeric input")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
